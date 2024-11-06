1d93fa3d9fb3e7a15648ee407e7599b102b37e91
tjdeveloper <tjmr1996@gmail.com>
2024-11-07 01:59:51 +0500

    fix: menu

--- BookShop/js/toggleMune.js
+++ BookShop/js/toggleMune.js
@@ -1,36 +1,42 @@
-// ==================== TOGGLE MENU ====================
-
 const overlay = document.getElementById('overlay');
-const navBurger = document.getElementById('nav-burger');
+const navBurgerEls = document.querySelectorAll('.nav__burger');
+const navCloseEls = document.querySelectorAll('.nav__close');
 const navMenu = document.getElementById('nav-actions');
+const headerEls = document.querySelectorAll('.header');
 
 const showMenu = () => {
   navMenu.classList.add('show-menu');
-  navBurger.classList.add('nav__burger--open');
   overlay.classList.remove('hidden');
   document.body.classList.add('no-scroll');
+  headerEls.forEach((headerEl) => headerEl.classList.remove('header__sticky'));
+  localStorage.setItem('headerSticky', 'false');
 };
 
 const hideMenu = () => {
   navMenu.classList.remove('show-menu');
-  navBurger.classList.remove('nav__burger--open');
   overlay.classList.add('hidden');
   document.body.classList.remove('no-scroll');
+  headerEls.forEach((headerEl) => headerEl.classList.add('header__sticky'));
+  localStorage.setItem('headerSticky', 'true');
 };
 
-navBurger.addEventListener('click', () => {
-  if (navMenu.classList.contains('show-menu')) {
-    hideMenu();
-  } else {
-    showMenu();
-  }
-});
+const stickyState = localStorage.getItem('headerSticky');
+if (stickyState === 'false') {
+  headerEls.forEach((headerEl) => headerEl.classList.remove('header__sticky'));
+} else {
+  headerEls.forEach((headerEl) => headerEl.classList.add('header__sticky'));
+}
 
-overlay.addEventListener('click', () => {
-  if (navMenu.classList.contains('show-menu')) {
-  hideMenu();
-  }
-});
+// Event Listeners
+navBurgerEls.forEach((navBurgerEl) =>
+  navBurgerEl.addEventListener('click', showMenu)
+);
+
+navCloseEls.forEach((navCloseEl) =>
+  navCloseEl.addEventListener('click', hideMenu)
+);
+
+overlay.addEventListener('click', hideMenu);
 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && navMenu.classList.contains('show-menu')) {
