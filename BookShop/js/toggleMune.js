18612960c55e929d7603a1e8139f38bfa0e50890
Жавлон Турдиматов <j.turdimatov@dkp.local>
2024-11-07 10:50:08 +0500

    fix: remove cart block in small devices

--- BookShop/js/toggleMune.js
+++ BookShop/js/toggleMune.js
@@ -1,39 +1,26 @@
 const overlay = document.getElementById('overlay');
-const navBurgerEls = document.querySelectorAll('.nav__burger');
-const navCloseEls = document.querySelectorAll('.nav__close');
+const navBurgerEl = document.getElementById('nav-burger');
+const navCloseEl = document.getElementById('nav-close');
 const navMenu = document.getElementById('nav-actions');
-const headerEls = document.querySelectorAll('.header');
+const headerEl = document.getElementById('header');
 
 const showMenu = () => {
   navMenu.classList.add('show-menu');
   overlay.classList.remove('hidden');
   document.body.classList.add('no-scroll');
-  headerEls.forEach((headerEl) => headerEl.classList.remove('header__sticky'));
-  localStorage.setItem('headerSticky', 'false');
+  headerEl.classList.remove('header__sticky');
 };
 
 const hideMenu = () => {
   navMenu.classList.remove('show-menu');
   overlay.classList.add('hidden');
   document.body.classList.remove('no-scroll');
-  headerEls.forEach((headerEl) => headerEl.classList.add('header__sticky'));
-  localStorage.setItem('headerSticky', 'true');
+  headerEl.classList.add('header__sticky');
 };
 
-const stickyState = localStorage.getItem('headerSticky');
-if (stickyState === 'false') {
-  headerEls.forEach((headerEl) => headerEl.classList.remove('header__sticky'));
-} else {
-  headerEls.forEach((headerEl) => headerEl.classList.add('header__sticky'));
-}
+navBurgerEl.addEventListener('click', showMenu);
 
-navBurgerEls.forEach((navBurgerEl) =>
-  navBurgerEl.addEventListener('click', showMenu)
-);
-
-navCloseEls.forEach((navCloseEl) =>
-  navCloseEl.addEventListener('click', hideMenu)
-);
+navCloseEl.addEventListener('click', hideMenu);
 
 overlay.addEventListener('click', hideMenu);
 
